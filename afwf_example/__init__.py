14bcae0907a14b590750b73182526f1e09033422
Sanhe Hu <husanhe@gmail.com>
2023-01-28 21:46:16 -0500

    add afwf_example python module

---
+++ afwf_example/__init__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+from ._version import __version__
+
+__author__ = "Sanhe Hu"
+__chore__ = "dc2ba0d33e28cbfd762ab8579bcb8483"
+
+# import afwf
+# from .handlers import python_version
+#
+# wf = afwf.Workflow()
+# wf.register(python_version.handler)
