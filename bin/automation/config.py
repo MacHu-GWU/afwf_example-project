84b8249b75febfd16636a074df45e6a3fd446296
Sanhe Hu <husanhe@gmail.com>
2023-01-29 03:53:10 -0500

    chore

--- bin/automation/config.py
+++ bin/automation/config.py
@@ -10,10 +10,12 @@ from pathlib_mate import Path
 
 @dataclasses.dataclass
 class AutomationConfig:
+    python_version: str = dataclasses.field()
     dir_workflow: Path = dataclasses.field()
 
 
 config = AutomationConfig(
+    python_version="3.8",
     dir_workflow=Path(
         "/Users/sanhehu/Documents/Alfred-Preferences/Alfred.alfredpreferences/workflows/user.workflow.BE825014-E97C-4213-BEFE-E652C1C83974"
     ),
